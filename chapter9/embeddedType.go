bc9a585fa84bf0953fad961fb88b25f07e8b4dfb
ZoNew <nitikarn12+git@gmail.com>
2020-05-12 00:21:43 +0700

    Embedded Type

---
+++ chapter9/embeddedType.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := new(Android)
+	a.Talk() //OR a.Person.Talk()
+	//sdaf
+}
+
+// Android is Person
+type Android struct {
+	Person //OR Person Person
+	Model  string
+}
+
+// Person is person
+type Person struct {
+	Name string
+}
+
+//Talk is method Print text
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
